228fe5ad8cf623c927b56e9171fc50a6a41015eb
Mehmet Oğuzhan Akçasoy <58490289+Oguzhanakcasoy@users.noreply.github.com>
2022-12-09 13:59:14 +0300

    Kariyer-Proje

---
+++ Kariyer.Proje.Entities/AppRole.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kariyer.Proje.Entities
+{
+    public class AppRole : BaseEntity
+    {
+        public string Definition { get; set; }
+
+        public List<AppUserRole> UserRoles { get; set; }
+    }
+}
---
+++ Kariyer.Proje/CustomValidationError.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kariyer.Proje
+{
+    public class CustomValidationError
+    {
+        public string PropertName { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
